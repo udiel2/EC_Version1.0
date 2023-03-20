5615fab00d2a7897cf95acd00d19b480f9cfa2cd
udiel2 <udiel2@ac.sce.ac.il>
2023-03-21 00:03:46 +0200

    HomeActivity update- fragments and binding toolbar on PantryFragment

--- app/src/main/java/com/application/easycook/HomeActivity.java
+++ app/src/main/java/com/application/easycook/HomeActivity.java
@@ -1,47 +1,71 @@
 package com.application.easycook;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.application.easycook.databinding.ActivityHomeBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import java.io.File;
-import java.io.IOException;
 
 public class HomeActivity extends AppCompatActivity {
     FirebaseFirestore firestore;
     StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-   ///ActivityMainBinding binding;
+   ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        StorageReference pathReference = storageRef.child("EZ_icons/cola.png");
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.navigation_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.navigation_pantry:
+                    replaceFragment(new PantryFragment());
+                    break;
+                case R.id.navigation_riceps:
+                    replaceFragment(new RecipeFragment());
+                    break;
+
+
+            }
+
+
+            return true;
+        });
         //gs://ezcookversion1.appspot.com/EZ_icons/cola.png
-        try {
-            File localfile= File.createTempFile("tempfile",".png");
-            pathReference.getFile(localfile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                    Bitmap bitmap= BitmapFactory.decodeFile(localfile.getAbsolutePath());
-                    ImageView imageView= findViewById(R.id.imageView);
-                    imageView.setImageBitmap(bitmap);
-                }
-            });
+        // StorageReference pathReference = storageRef.child("EZ_icons/cola.png");
+//        try {
+//            File localfile= File.createTempFile("tempfile",".png");
+//            pathReference.getFile(localfile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+//                @Override
+//                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+//                    Bitmap bitmap= BitmapFactory.decodeFile(localfile.getAbsolutePath());
+//                    ImageView imageView= findViewById(R.id.imageView);
+//                    imageView.setImageBitmap(bitmap);
+//                }
+//            });
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
-
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.home_fragment_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
