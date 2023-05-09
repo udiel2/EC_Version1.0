b8f7feb49fbfd1ebc60e7d84bc38a16a0aa50c6f
Himbert Liron <lironbenharrouch@gmail.com>
2023-05-09 20:38:24 +0300

    d

---
+++ app/src/main/java/com/application/easycook/McActivity.java
@@ -0,0 +1,14 @@
+package com.application.easycook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class McActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mc);
+    }
+}
