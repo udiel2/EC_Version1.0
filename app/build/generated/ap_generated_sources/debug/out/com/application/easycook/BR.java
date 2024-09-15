8574bf104bb104fd646a5de4b527e5c28db7df50
Himbert Liron <lironbenharrouch@gmail.com>
2024-09-15 17:26:53 +0300

    Refresh
    Reshesh the app

---
+++ app/build/generated/ap_generated_sources/debug/out/com/application/easycook/BR.java
@@ -0,0 +1,5 @@
+package com.application.easycook;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ app/build/generated/data_binding_trigger/debug/com/application/easycook/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.application.easycook;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
